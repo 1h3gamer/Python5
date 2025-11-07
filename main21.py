bff31b32298cae65fb4da9cd58233bf590a072a4
1h3gamer <Idaune.nathan@gmail.com>
2025-11-07 18:07:18 +0000

    square

---
+++ main21.py
@@ -0,0 +1,22 @@
+num1 = (8)
+num2 = (9)
+num3 = (7)
+
+
+square1 = (num1 * num1)
+square2 = (num2 * num2)
+square3 = (num3 * num3)
+if (square1)%2 == 0:
+    print ("even")
+else:
+    print("odd")
+
+if (square2)%2 == 0:
+    print ("even")
+else:
+    print("odd")
+
+if (square3)%2 == 0:
+    print ("even")
+else:
+    print("odd")
