ccc69fca12dfcb2dbf4c3ec9eea6746bcb58c504
1h3gamer <Idaune.nathan@gmail.com>
2025-11-13 20:04:28 +0000

    hw frequency

---
+++ main23.py
@@ -0,0 +1,27 @@
+test_scores = {
+    'Codingal' : '3',
+    'is' : '2',
+    'best' : '2',
+    'for' : '2',
+    'Coding' : '1'
+}
+
+
+print("Test scores for Codingal")
+print(test_scores.get('Codingal','Not found'))
+
+
+print("Test scores for is")
+print(test_scores.get('is','Not found'))
+
+
+print("Test scores for best")
+print(test_scores.get('best','Not found'))
+
+
+print("Test scores for for")
+print(test_scores.get('for','Not found'))
+
+
+print("Test scores for Coding")
+print(test_scores.get('Coding','Not found'))
